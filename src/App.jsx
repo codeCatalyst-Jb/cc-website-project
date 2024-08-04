514b961806a7810a929178f75c1015268dc7a68e
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-08-04 01:25:53 -0700

    Working on Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import HeroSection from "./components/HeroSection/HeroSection";
 import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div id="app">
       <Navbar />
+      <HeroSection />
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,21 +3,21 @@ import React from "react";
 const Navbar = () => {
   return (
     <header>
-      <nav className="flex items-center justify-between text-white py-5">
+      <nav className="flex items-center justify-between text-white py-5 px-8">
         <div className="nav-head">
           <h1>
             Code<span> Catalyst</span>
           </h1>
         </div>
         <div id="nav-menu" className="flex gap-24 items-center">
-          <ul id="menuItem" className="flex gap-6">
+          <ul id="menuItem" className="flex gap-8 cursor-pointer">
             <li>Home</li>
             <li>About Us</li>
             <li>Services</li>
           </ul>
           <div id="nav-btn" className="flex gap-6">
-            <button>Sign in</button>
-            <button>Connect</button>
+            <button className="border-2 rounded-xl py-2 px-4">Sign in</button>
+            <button className="border-2 rounded-xl py-2 px-4">Connect</button>
           </div>
         </div>
       </nav>
