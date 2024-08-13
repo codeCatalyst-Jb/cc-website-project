8d4b96966c73be4340037a1be033df0e51aa5db5
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-08-13 03:43:13 -0700

    Working on making navbar responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,49 +1,80 @@
-import React from 'react'
-import Img from '../../assets/cclogo2-.png'
-import './footer.css'
+import React from "react";
+import Img from "../../assets/cclogo2-.png";
+import "./footer.css";
 
 const Footer = () => {
   return (
-   
-   <>
-   <footer className='bg-gray-900 h-25vh border-white flex flex-wrap gap-5 text-gray-600 justify-evenly items-center px-6 py-5'>
-<div className="about">
-  <img src={Img} alt=""  className='w-1/5'/>
-  <p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta harum similique deserunt distinctio quisquam odio quas. Animi quaerat qui alias consequatur. Aperiam debitis beatae provident deleniti a minima, nemo quam quas, deserunt aliquid sit fuga excepturi accusamus, neque officia ipsum?</p>
-</div>
+    <>
+      <footer className="bg-gray-900 h-25vh border-white flex flex-wrap gap-5 text-gray-600 justify-evenly items-center px-6 py-5">
+        <div className="about">
+          <img src={Img} alt="" className="w-1/5" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+            harum similique deserunt distinctio quisquam odio quas. Animi
+            quaerat qui alias consequatur. Aperiam debitis beatae provident
+            deleniti a minima, nemo quam quas, deserunt aliquid sit fuga
+            excepturi accusamus, neque officia ipsum?
+          </p>
+        </div>
 
-<div className="qlinks">
-  <h2 className='text-blue-500'>Quick links</h2>
-<ul>
-  <li><a href=""> Home</a></li>
-  <li><a href=""> About</a></li>
-  <li><a href="">Events</a></li>
-  <li><a href="">Teams</a></li>
-  <li><a href="">Get in touch</a></li>
-  <li><a href="">Site Map</a></li>
-</ul>
-</div>
+        <div className="qlinks">
+          <h2 className="text-blue-500">Quick links</h2>
+          <ul>
+            <li>
+              <a href=""> Home</a>
+            </li>
+            <li>
+              <a href=""> About</a>
+            </li>
+            <li>
+              <a href="">Events</a>
+            </li>
+            <li>
+              <a href="">Teams</a>
+            </li>
+            <li>
+              <a href="">Get in touch</a>
+            </li>
+            <li>
+              <a href="">Site Map</a>
+            </li>
+          </ul>
+        </div>
 
-<div className="social">
-<h2 className='text-blue-500'>FOLLOW US</h2>
-<ul>
-  <li><a href="">Instagram</a></li>
-  <li><a href="">Linkedin</a></li>
-  <li><a href="">Github</a></li>
-  <li><a href="">Discord</a></li>
-</ul>
-</div>
+        <div className="social">
+          <h2 className="text-blue-500">FOLLOW US</h2>
+          <ul>
+            <li>
+              <a href="">Instagram</a>
+            </li>
+            <li>
+              <a href="">Linkedin</a>
+            </li>
+            <li>
+              <a href="">Github</a>
+            </li>
+            <li>
+              <a href="">Discord</a>
+            </li>
+          </ul>
+        </div>
 
-<div className="blog">
-<h2> Do you want to read some Blog offered by us</h2>
-<a href="" className='text-blue-500'>Our Blogs</a>
-<p >Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, enim.</p>
-</div>
-   </footer>
-   <h2 className='bg-gray-700 text-center'>Designed by <span className='text-white'>Code Catalyst Team</span> 
-   <h3>All Rights reserved to Code Catalyst</h3></h2>
-   </>
-  )
-}
+        <div className="blog">
+          <h2> Do you want to read some Blog offered by us</h2>
+          <a href="" className="text-blue-500">
+            Our Blogs
+          </a>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, enim.
+          </p>
+        </div>
+      </footer>
+      <h3 className="bg-gray-700 text-center">
+        Designed by <span className="text-white">Code Catalyst Team</span>
+        <h2>All Rights reserved to Code Catalyst</h2>
+      </h3>
+    </>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,18 @@
-import React from "react";
-import Img from '../../assets/cclogo2-.png'
+import React, { useState } from "react";
+import Img from "../../assets/cclogo2-.png";
+import "./Navbar.css";
+
 const Navbar = () => {
+  const [showMediaIcons, setShowMediaIcons] = useState(false);
+
   return (
-    <header>
-      <nav className="flex items-center justify-between text-white px-8">
+    <header id="navbar">
+      <nav
+        id="navbar-wrapper"
+        className="flex items-center justify-between text-white px-8"
+      >
         <div className="nav-head">
-         <img src={Img} alt="" className="w-1/5" />
+          <img src={Img} alt="" className="w-1/5" />
         </div>
         <div id="nav-menu" className="flex gap-24 items-center">
           <ul id="menuItem" className="flex gap-8 cursor-pointer">
@@ -14,8 +21,20 @@ const Navbar = () => {
             <li>Services</li>
           </ul>
           <div id="nav-btn" className="flex gap-6">
-            <button className="border-2 rounded-xl py-2 px-4 border-blue-500">Sign in</button>
-            <button className="border-2 rounded-xl py-2 px-4 border-blue-500">Connect</button>
+            <button className="contact-navBtn border-2 rounded-xl py-2 px-4 border-blue-500">
+              Sign in
+            </button>
+            <button className="contact-navBtn border-2 rounded-xl py-2 px-4 border-blue-500">
+              Connect
+            </button>
+          </div>
+        </div>
+
+        <div className="nav-hamMenu">
+          <div className="hamburger-menu">
+            <a href="#" onClick={() => setShowMediaIcons(!showMediaIcons)}>
+              <i className="fa-solid fa-bars text-white text-2xl"></i>
+            </a>
           </div>
         </div>
       </nav>
