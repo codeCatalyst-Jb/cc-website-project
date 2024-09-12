4d6295528a54e2660fd174461751fa577dccb1da
Aman-web-dev <amanzhx1234+github@gmail.com>
2024-09-12 12:23:15 +0530

    Uiupdate

--- src/Sections/HeroSection/HeroSection.jsx
+++ src/Sections/HeroSection/HeroSection.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import "./HeroSection.css";
+// React
+import { motion } from "framer-motion"
 
 const HeroSection = () => {
   return (
     <div id="HeroSection" className="w-full h-screen items-center flex flex-col justify-center gap-8">
+      {/* <img src="src/assets/emojistar 1.png" alt="Emoji Star" className="w-48 md:w-80 mr-16 mb-48" /> */}
+      <motion.div
+      className="box"
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+    />
       <h1 className="font-semibold text-6xl sm:text-4xl md:text-[165px] lg:text-10xl pb-9 dark:text-white ">
         Code Catalyst
       </h1>
